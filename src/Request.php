d17942d661834fc829e1d94c797bd28290dc006b
kahalian <kahalian@gmail.com>
2021-11-24 00:44:50 +0330

    add order_id function

--- src/Request.php
+++ src/Request.php
@@ -2,87 +2,46 @@
 
 namespace Saber\Vandapay;
 
-use Illuminate\Support\Facades\Http;
-
-class Request
+class Vandapay
 {
     /** @var string */
     private $pin_api;
 
     /** @var int */
-    private $order_id;
-
-    /** @var int */
     private $amount;
 
     /** @var string */
-    private $description;
+    private $order_id;
 
-    /** @var string */
-    private $callbackUrl;
-
-    /** @var string */
-    private $mobile;
-
-    /** @var string */
-    private $email;
-
-    public function __construct(string $pin_api, int $amount, string $order_id)
+    public function pin_api(string $pin_api): self
     {
-        $this->pin_api      = $pin_api;
-        $this->amount       = $amount;
-        $this->order_id     = $order_id;
-    }
-
-    public function send(): RequestResponse
-    {
-        $url = 'https://vandapardakht.com/Request';
-
-        $data = [
-            'pin'           => $this->pin_api,//your api
-            'price'         =>$this->amount,
-            'callback'      =>$this->callbackUrl ,//your callback with snverify.php
-            'order_id'      => $this->order_id,
-            "email"         => $this->email,//your customer's email optional
-            "description"   => $this->description,// your  description  optional
-            "name"          =>"Reza",//your customer's name  optional
-            "mobile"        =>$this->mobile,//your customer's mobile  optional
-            'ip'            => $_SERVER['REMOTE_ADDR'],
-            'callback_type' =>2
-        ];
-
-
-
-        $response = Http::asJson()->acceptJson()->post($url, $data);
-
-        return new RequestResponse($response->json());
-    }
-
-    public function description(string $description): self
-    {
-        $this->description = $description;
+        $this->pin_api = $pin_api;
 
         return $this;
     }
 
-    public function callbackUrl(string $callbackUrl): self
+    public function amount(int $amount): self
     {
-        $this->callbackUrl = $callbackUrl;
+        $this->amount = $amount;
 
         return $this;
     }
 
-    public function mobile(string $mobile): self
+    public function order_id(string $order_id): self
     {
-        $this->mobile = $mobile;
+        $this->order_id = $order_id;
 
         return $this;
     }
-
-    public function email(string $email): self
+    public function request(): Request
     {
-        $this->email = $email;
+        $pin_api = $this->pin_api ?: config('vandapay.pin_api');
+        return new Request($pin_api, $this->amount,$this->order_id);
+    }
 
-        return $this;
+    public function verification(): Verification
+    {
+        $pin_api = $this->pin_api ?: config('vandapay.pin_api');
+        return new Verification($pin_api, $this->amount);
     }
 }
