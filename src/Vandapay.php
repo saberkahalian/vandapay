af8212bda7493afedba863af76bfc5472998eed6
kahalian <kahalian@gmail.com>
2021-11-23 05:20:12 +0330

    Update Vandapay.php

--- src/Vandapay.php
+++ src/Vandapay.php
@@ -10,6 +10,9 @@ class Vandapay
     /** @var int */
     private $amount;
 
+    /** @var string */
+    private $order_id;
+
     public function pin_api(string $pin_api): self
     {
         $this->pin_api = $pin_api;
@@ -24,10 +27,16 @@ class Vandapay
         return $this;
     }
 
+    public function order_id(string $order_id): self
+    {
+        $this->order_id = $order_id;
+
+        return $this;
+    }
     public function request(): Request
     {
         $pin_api = $this->pin_api ?: config('vandapay.pin_api');
-        return new Request($pin_api, $this->amount);
+        return new Request($pin_api, $this->amount,$this->order_id);
     }
 
     public function verification(): Verification
