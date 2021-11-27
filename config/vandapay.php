3d5e73fc16d026d1c40a1017e63ccf69d36697ce
kahalian <kahalian@gmail.com>
2021-11-27 19:07:34 +0330

    Update vandapay.php

--- config/vandapay.php
+++ config/vandapay.php
@@ -8,6 +8,13 @@ return [
     */
 
     'pin_api' => env('VANDA_PIN_API', 'vp-00000'),
+    
+    /*
+    *   VandaPay request url
+    *
+    */
+
+    'request_url' => env('REQUEST_URL', 'https://www.vandapardakht.com/index.php/Request'),
 
 
 ];
