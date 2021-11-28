e928c1d2a2c6c3ceeefb93c71b6aa2c26d6f8043
kahalian <kahalian@gmail.com>
2021-11-28 15:50:11 +0330

    Update vandapay.php

--- config/vandapay.php
+++ config/vandapay.php
@@ -7,7 +7,8 @@ return [
     *
     */
 
-    'pin_api' => env('VANDA_PIN_API', 'vp-00000'),
+    'pin_api' => env('VANDA_PIN_API', 'Vp_37324fcefe793'),
+
     
     /*
     *   VandaPay request url
@@ -15,6 +16,28 @@ return [
     */
 
     'request_url' => env('REQUEST_URL', 'https://www.vandapardakht.com/index.php/Request'),
+
+    /*
+    *   VandaPay verification url
+    *
+    */
+
+    'verification_url' => env('VERIFICATION_URL', 'https://www.vandapardakht.com/index.php/Verify'),
+
+
+    /*
+    *   has datatable ? 
+    *
+    */
+
+    'store_invoice' => env('STORE_INVOICE', true),
+
+    /*
+    *   invoice table name
+    *
+    */
+
+    'invoice_model' => env('INVOICE_MODEL', 'App\\Models\\Invoice'),
 
 
 ];
